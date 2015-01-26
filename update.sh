a2660626ec86d7e4107834c00b89f4e0c3436e62
Tim Green <greent@vmware.com>
2015-01-26 11:11:22 -0500

    add init script and merge latest submodule updates

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ MODULES='ctrlp.vim delimitMate nerdtree tagbar syntastic'
 
 for module in $MODULES
 do
-  (cd $PWD/bundle/$module && git pull && git submodule update)
+  (cd $PWD/bundle/$module && git fetch && git merge origin/master)
   if [ $? -ne 0 ]; then
     echo "[ERROR]: unable to update module $module"
     exit 1
